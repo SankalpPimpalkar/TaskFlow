d9ca56163519f2ee9328b42226ded797f5609c25
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2025-10-17 19:52:41 +0530

    Added dev and prod env configuration setup

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
 import { configDotenv } from "dotenv";
-configDotenv()
+configDotenv({ path: `.env.${process.env.NODE_ENV || 'development'}` })
 
 export const PORT = process.env.PORT || 5050
+export const NODE_ENV = process.env.NODE_ENV
 
 export const DB_NAME = process.env.DB_NAME
 export const DB_URL = process.env.DB_URL
